71c0bbc48b39698fcf863a5f5a3c36e75cbcdc39
kasimirlindholm-stack <kasimir.lindholm@gmail.com>
2026-09-25 14:37:32 +0300

    Build 22 shell: cache-bust remote app URL

--- capacitor.config.js
+++ capacitor.config.js
@@ -3,7 +3,7 @@ const config = {
   appName: 'Hoods of Helsinki',
   webDir: 'www',
   server: {
-    url: 'https://kasimirlindholm-stack.github.io/Hoods-of-Helsinki/',
+    url: 'https://kasimirlindholm-stack.github.io/Hoods-of-Helsinki/?native=1&shell=22',
     cleartext: false
   },
   android: { backgroundColor: '#090b0a' }
