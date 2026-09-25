8c20684ba13858b78c984ecbdf6e953eb552fef2
kasimirlindholm-stack <kasimir.lindholm@gmail.com>
2026-09-25 13:54:07 +0300

    Use Build 14 PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hoods-build-13';
+const CACHE='hoods-build-14';
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{if(response&&response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{})}return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
