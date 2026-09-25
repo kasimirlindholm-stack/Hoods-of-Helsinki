2534d4a73cc50e459c7e8fb8c15b21692c832c72
kasimirlindholm-stack <kasimir.lindholm@gmail.com>
2026-09-25 13:56:16 +0300

    Configure Hoods Android app shell

---
+++ capacitor.config.js
@@ -0,0 +1,11 @@
+const config = {
+  appId: 'fi.hoodsofhelsinki.game',
+  appName: 'Hoods of Helsinki',
+  webDir: 'www',
+  server: {
+    url: 'https://kasimirlindholm-stack.github.io/Hoods-of-Helsinki/',
+    cleartext: false
+  },
+  android: { backgroundColor: '#090b0a' }
+};
+module.exports = config;
